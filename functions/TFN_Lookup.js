501a14bec17b7d4002f4f4e5ae146ee9cd2cc50f
veer9582 <vedwivedi@convergentusa.com>
2020-12-30 01:34:43 +0530

    getact

--- functions/TFN_Lookup.js
+++ functions/TFN_Lookup.js
@@ -2,6 +2,6 @@ exports.handler = function(context, event, callback) {
     const twiml = new Twilio.twiml.VoiceResponse();
     twiml.say('Hello World!');
     callback(null, twiml);
-    //for test again
+    //for test again getaccountinfo
   };
   
