79c6dbb063d098e2ef8bd5b3dea20b4459ca11e0
veer9582 <vedwivedi@convergentusa.com>
2020-12-30 01:12:40 +0530

    8754

--- functions/TFN_Lookup.js
+++ functions/TFN_Lookup.js
@@ -1,1 +1,6 @@
-//git testing
+exports.handler = function(context, event, callback) {
+    const twiml = new Twilio.twiml.VoiceResponse();
+    twiml.say('Hello World!');
+    callback(null, twiml);
+  };
+  
