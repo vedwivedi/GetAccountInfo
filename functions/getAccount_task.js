e8c16ae6cddd2e9a5a42d8791f206f62f280bb60
veer9582 <vedwivedi@convergentusa.com>
2021-01-18 22:54:28 +0530

    Your comment

--- functions/getAccount_task.js
+++ functions/getAccount_task.js
@@ -59,12 +59,12 @@ exports.getAccount_task =async function(context, event, callback,RB) {
 
    let squestion = `Please Say or enter your account number starting with ${Remember.clientData.F_Letter_Namespace}, located ${sMsg}. Enter the numbers  after the letter ${Remember.clientData.F_Letter_Namespace}.`; 
    
-   if(Memory.AccountFailed_Counter != undefined){
-        console.log("MemorygetAccount_task_counter; " +Memory.AccountFailed_Counter);
-        if(Memory.AccountFailed_Counter == 1){
-          squestion = "I did not understand. , " +squestion;
-        }
-      }
+  //  if(Memory.AccountFailed_Counter != undefined){
+  //       console.log("MemorygetAccount_task_counter; " +Memory.AccountFailed_Counter);
+  //       if(Memory.AccountFailed_Counter == 1){
+  //         squestion = "I did not understand. , " +squestion;
+  //       }
+  //     }
    
    let bPhone = false;
    if(Memory.AccountFrom == "Phone"){
